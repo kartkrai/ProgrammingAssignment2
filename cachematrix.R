3de974fdb378ccdf8e4f183826e383ecf3218474
kartkrai <37115655+kartkrai@users.noreply.github.com>
2020-05-24 12:21:45 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,27 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+  
+  cofact<- x     ## declaring cofactor matrix 
+  d<- c(1:nrow(x))
+  cacheSolve(x, cofact, d)
 }
-
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,cofact, d) {
+  
+  for (a in d) {
+    
+    for (b in d) {
+      if((b+a)%%2==0)
+        cofact[a,b]<- (det(x[-a,-b]))   ## calculating cofactor matrix 
+      if((b+a)%%2==1)
+        cofact[a,b]<- -(det(x[-a,-b]))   ## calculating cofactor matrix 
+    }
+    
+  }
+  
+  cofact/(det(x))     ## Returns a matrix that is the inverse of 'x'
 }
